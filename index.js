3b015ec19c907413b7e97702389f585fadf44293
andro ostoic <androstoic@gmail.com>
2019-01-10 12:12:47 -0300

    login listo

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-var ingresarAdmin = document.getElementById("myForm").submit();
+var ingresarAdmin = document.getElementById('signIn');
+
+
 
 
 ingresarAdmin.addEventListener('click', function() {
-    document.getElementById('BarraCargando').style.display = 'block';
-    document.getElementById('signIn').style.display = 'none';
 
-    var email = document.getElementById('correo').value;
-    var password = document.getElementById('password').value;
+    console.log("ola");
+   // document.getElementById('BarraCargando').style.display = 'block';
+    //document.getElementById('signIn').style.display = 'none';
+
+    var email = document.getElementById('inputEmail').value;
+    email = email +'@abcs.cl';
+    console.log(email)
+    var password = document.getElementById('inputPassword').value;
 
      firebase.auth().signInWithEmailAndPassword(email, password).then(
       function(s){
-         console.log(s);
-    firebase.database().ref('/recepcionistas/' + s.uid).once('value').then(function(snapshot) {
+         console.log(s.user);
+    firebase.database().ref('/admin/' + s.user.uid).once('value').then(function(snapshot) {
    if (snapshot.val() != null)
     window.location.href = 'app';
     else {
