7d4237d76651424b83cbaa026a8a85c02ae5ab46
andro ostoic <androstoic@gmail.com>
2019-01-10 17:09:51 -0300

    buen trabajo hoy

--- app/app.js
+++ app/app.js
@@ -3,9 +3,11 @@
 // Declare app level module which depends on views, and core components
 angular.module('myApp', [
   'ngRoute',
+  'ngMaterial',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
+
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,18 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', [function($scope) {
+  const firestore = firebase.firestore();
+  const settings = {/* your settings... */ timestampsInSnapshots: true};
+  firestore.settings(settings);
+
+
+
+  firestore.collection("clients").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id);
+       // console.log(`${doc.id} => ${doc.data()}`);
+    });
+});
 
 }]);
