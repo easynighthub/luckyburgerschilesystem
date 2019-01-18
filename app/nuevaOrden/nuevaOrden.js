d59df283dbc00397c1e48e6585327f0003c1898a
Andro Slodoban Ostoic Munoz <aostoic@conicyt.cl>
2019-01-18 15:59:51 -0300

    push array update

--- app/nuevaOrden/nuevaOrden.js
+++ app/nuevaOrden/nuevaOrden.js
@@ -2,89 +2,102 @@
 
 angular.module('myApp.nuevaOrden', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/nuevaOrden', {
-    templateUrl: 'nuevaOrden/nuevaOrden.html',
-    controller: 'NuevaOrdenCtrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/nuevaOrden', {
+            templateUrl: 'nuevaOrden/nuevaOrden.html',
+            controller: 'NuevaOrdenCtrl'
+        });
+    }])
 
-.controller('NuevaOrdenCtrl', ['$scope','$firebaseArray',function($scope,$firebaseArray) {
+    .controller('NuevaOrdenCtrl', ['$scope', '$firebaseArray', function ($scope, $firebaseArray) {
 
-  $(dashboard).addClass("active");
-  $(clientes).removeClass("active");
+        $(dashboard).addClass("active");
+        $(clientes).removeClass("active");
+
+        $scope.pedido = [];
+        traerClientes();
+
+        function traerClientes() {
+            var buscarClientes = db.ref().child('clients');
+            var buscarClientesER = $firebaseArray(buscarClientes);
+            buscarClientesER.$loaded().then(function () {
+                console.log(buscarClientesER);
+                $scope.clientes = buscarClientesER;
+                $scope.simulateQuery = false;
+                $scope.isDisabled = false;
+                $scope.repos = loadAll();
+                $scope.querySearch = querySearch;
+                $scope.selectedItemChange = selectedItemChange;
+                $scope.searchTextChange = searchTextChange;
+
+                function querySearch(query) {
+                    if (!query) return;
+                    var results = query ? $scope.repos.filter(createFilterFor(query)) : $scope.repos,
+                        deferred;
+                    if ($scope.simulateQuery) {
+                        deferred = $q.defer();
+                        $timeout(function () { deferred.resolve(results); }, Math.random() * 1000, false);
+                        return deferred.promise;
+                    } else {
+                        return results;
+                    }
+                }
+
+                function searchTextChange(text) {
+                    console.log('Text changed to ' + text);
+                }
+
+                function selectedItemChange(item) {
+                    console.log('Item changed to ' + JSON.stringify(item));
+                    $scope.pedido.userId = item.$id;
+
+                    $scope.nombre = item.nombre;
+                    $scope.apellido= item.apellido;
+                    $scope.correo= item.correo;
+                    $scope.direccion= item.value;
 
 
-  traerHabitaciones();
 
-  function traerHabitaciones() {
-    var buscarClientes = db.ref().child('clients');
-    var buscarClientesER = $firebaseArray(buscarClientes);
-    buscarClientesER.$loaded().then(function () {
-        console.log(buscarClientesER);
-        $scope.clientes = buscarClientesER;
-        $scope.simulateQuery = false;
-        $scope.isDisabled    = false;
-        $scope.repos         = loadAll();
-        $scope.querySearch   = querySearch;
-        $scope.selectedItemChange = selectedItemChange;
-        $scope.searchTextChange   = searchTextChange;
 
-        function querySearch (query) {
-            if (!query) return;
-            var results = query ? $scope.repos.filter( createFilterFor(query) ) : $scope.repos,
-                deferred;
-            if ($scope.simulateQuery) {
-                deferred = $q.defer();
-                $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
-                return deferred.promise;
-            } else {
-                return results;
-            }
-        }
+                }
 
-        function searchTextChange(text) {
-            console.log('Text changed to ' + text);
-        }
+                /**
+                 * Build `components` list of key/value pairs
+                 */
+                function loadAll() {
+                    var repos = $scope.clientes;
+                    return repos.map(function (repo) {
+                        repo.value = repo.telefono;
+                       // repo.value = repo.telefono.toLowerCase();
+                        return repo;
+                    });
+                }
 
-        function selectedItemChange(item) {
-            console.log('Item changed to ' + JSON.stringify(item));
-            $scope.reserva.habitacionId = item;
+                /**
+                 * Create filter function for a query string
+                 */
+                function createFilterFor(query) {
+                    console.log(query);
+                    console.log("ho");
+                    var lowercaseQuery = query;
+                    //  var lowercaseQuery = angular.lowercase(query);
 
-        }
+                    return function filterFn(item) {
+                        console.log(item);
+                        return (item);
+                    };
 
-        /**
-         * Build `components` list of key/value pairs
-         */
-        function loadAll() {
-            var repos = $scope.clientes;
-            return repos.map( function (repo) {
-                repo.value = repo.nombre.toLowerCase();
-                return repo;
+                }
+
+
+
             });
-        }
-
-        /**
-         * Create filter function for a query string
-         */
-        function createFilterFor(query) {
-            var lowercaseQuery = angular.lowercase(query);
-
-            return function filterFn(item) {
-                return (item.value.indexOf(lowercaseQuery) === 0);
-            };
 
         }
 
 
 
-    });
-
-}
 
 
 
-
-
-
-}]);
+    }]);
